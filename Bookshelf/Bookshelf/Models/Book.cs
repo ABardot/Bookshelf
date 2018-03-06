515abd0a25e2b18b2908b65fd016a27665785c4f
ABardot <aabardot5@gmail.com>
2018-03-06 10:24:15 -0500

    Added Category Class and prop

--- Bookshelf/Bookshelf/Models/Book.cs
+++ Bookshelf/Bookshelf/Models/Book.cs
@@ -1,27 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
-
 
 namespace Bookshelf.Models
 {
     public class Book
     {
         [Key]
-        public int Id { get; set; }
-        public int BookNunber { get; set; }
+        public int BookID { get; set; }
         public string Title { get; set; }
         public DateTime PublishedDate { get; set; }
         public string Description { get; set; }
         public string ISBN { get; set; }
-        
 
-        [ForeignKey("Category")]
+        [ForeignKey("Category")] // Set up the foreign key for relationship
         public int CategoryID { get; set; }
-        public virtual Category Catagory { get; set; }
+        public virtual Category Catagory { get; set; } // A navigation property with one category 
     }
 }
